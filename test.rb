c924d18f84de77183afe759e886318133387e69b
Satoshi Egi <egi@egison.org>
2014-02-21 10:04:21 +0900

    update

--- test.rb
+++ test.rb
@@ -1,7 +1,7 @@
 require 'egison'
 
-match([1, 2, 3, 4, 5]) do
-  with(_[*_, *a, *_], guard { a.inject(:*) == 12 }) do
-    a #=> [3, 4]
+match([1, 2, 3, 4, 5, 6, 7]) do
+  with(_[*_, 2, *a, 6, *_]) do
+    p a
   end
 end

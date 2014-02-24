c7b62e308506cf07be3853a7ab65e9d0ee6359c5
Satoshi Egi <egi@egison.org>
2014-02-24 10:29:31 +0900

    update

--- test.rb
+++ test.rb
@@ -2,25 +2,25 @@
 require 'egison'
 
 match(1) do
-  with(a) do
+  with(_a) do
     p a
   end
 end
 
 match([1, 2, 3, 4, 5, 6, 7]) do
-  with(Array.(*_, 2, *a, 6, *_)) do
+  with(Array.(*_, 2, *_a, 6, *_)) do
     p a
   end
 end
 
 match([1, 2, 3, 4, 5, 6, 7]) do
-  with(_[*_, 2, *a, 6, *_]) do
+  with(_[*_, 2, *_a, 6, *_]) do
     p a
   end
 end
 
 match([[1, 2], [3, 4], [5, 6]]) do
-  with(_[_, _[a, b], _]) do
+  with(_[_, _[_a, _b], _]) do
     p(a, b)
   end
 end

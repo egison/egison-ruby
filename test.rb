f4ab4198c64c7881db7a58de3aa6d5ed5eb6d274
Satoshi Egi <egi@egison.org>
2014-05-02 14:59:16 +0900

    add nested tet

--- test.rb
+++ test.rb
@@ -11,3 +11,7 @@ p(match_all(tgt) do with(List.(_a, *_, __a, *_)) { a } end)
 p(match_all(tgt) do with(Multiset.(_a, *_b)) { [a, b] } end)
 
 p(match_all(tgt) do with(Multiset.(_a, __a, *_)) { a } end)
+
+tgt2 = [[1, 2], [3, 1], [4, 3]]
+
+p(match_all(tgt2) do with(List.(_a, List.(_b, _), _)) { [a, b] } end)

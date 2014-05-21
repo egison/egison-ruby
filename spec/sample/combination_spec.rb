6fc1aeaccbfd061b8a2eee9af9220652ab9dae16
xyx_is <god.doesnotnecessarilyplaydice@gmail.com>
2014-05-21 23:25:52 +0900

    convert samples into tests of rspec

---
+++ spec/sample/combination_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require 'egison'
+
+describe "sample" do
+  describe "combination.rb" do
+    it %q{match_all([1,2,3,4,5]) do with(List.(*_, _x, *_, _y, *_)) { [x, y] } end} do
+      expect(match_all([1,2,3,4,5]) do with(List.(*_, _x, *_, _y, *_)) { [x, y] } end).to eq \
+        [[1, 2], [1, 3], [1, 4], [1, 5], [2, 3], [2, 4], [2, 5], [3, 4], [3, 5], [4, 5]]
+    end
+    it %q{match_all([1,2,3,4,5]) do with(List.(*_, _x, *_, _y, *_, _z, *_)) { [x, y, z] } end} do
+      expect(match_all([1,2,3,4,5]) do with(List.(*_, _x, *_, _y, *_, _z, *_)) { [x, y, z] } end).to eq \
+        [[1, 2, 3], [1, 2, 4], [1, 2, 5], [1, 3, 4], [1, 3, 5], [1, 4, 5], [2, 3, 4], [2, 3, 5], [2, 4, 5], [3, 4, 5]]
+    end
+  end
+end
+
---
+++ spec/sample/poker_hands_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+require 'egison'
+
+alias match_single match
+def poker_hands cs
+  match_single(cs) do
+    with(Multiset.(_[_s, _n], _[__s, __("n+1")], _[__s, __("n+2")], _[__s, __("n+3")], _[__s, __("n+4")])) do
+      "Straight flush"
+    end
+    with(Multiset.(_[_, _n], _[_, __n], _[_, __n], _[_, __n], _)) do
+      "Four of kind"
+    end
+    with(Multiset.(_[_, _m], _[_, __m], _[_, __m], _[_, _n], _[_, __n])) do
+      "Full house"
+    end
+    with(Multiset.(_[_s, _], _[__s, _], _[__s, _], _[__s, _], _[__s, _])) do
+      "Flush"
+    end
+    with(Multiset.(_[_, _n], _[_, __("n+1")], _[_, __("n+2")], _[_, __("n+3")], _[_, __("n+4")])) do
+      "Straight"
+    end
+    with(Multiset.(_[_, _n], _[_, __n], _[_, __n], _, _)) do
+      "Three of kind"
+    end
+    with(Multiset.(_[_, _m], _[_, __m], _[_, _n], _[_, __n], _)) do
+      "Two pairs"
+    end
+    with(Multiset.(_[_, _n], _[_, __n], _, _, _)) do
+      "One pair"
+    end
+    with(Multiset.(_, _, _, _, _)) do
+      "Nothing"
+    end
+  end
+end
+
+describe "sample" do
+  describe "poker.rb" do
+    it "Straight flush" do
+      expect(poker_hands([["diamond", 1], ["diamond", 3], ["diamond", 5], ["diamond", 4], ["diamond", 2]])).to eq "Straight flush"
+    end
+    it "Full house" do
+      expect(poker_hands([["diamond", 1], ["club", 2], ["club", 1], ["heart", 1], ["diamond", 2]])).to eq "Full house"
+    end
+    it "Straight" do
+      expect(poker_hands([["diamond", 4], ["club", 2], ["club", 5], ["heart", 1], ["diamond", 3]])).to eq "Straight"
+    end
+    it "Nothing" do
+      expect(poker_hands([["diamond", 4], ["club", 10], ["club", 5], ["heart", 1], ["diamond", 3]])).to eq "Nothing"
+    end
+  end
+end
+

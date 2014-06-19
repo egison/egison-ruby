35ff1bfa4220fba49af3f6ef4a7b03456359b747
antimon2 <antimon2.me@gmail.com>
2014-06-20 07:21:41 +0900

    fix stream / refactoring

--- spec/sample/stream_spec.rb
+++ spec/sample/stream_spec.rb
@@ -34,10 +34,24 @@ describe "sample" do
         [[1, 2], [1, 3], [2, 1], [1, 4], [2, 3], [3, 1], [1, 5], [2, 4], [3, 2], [4, 1]]
     end
 
+    # (take 20 (match-all nats (multiset integer) [<cons $m <cons $n _>> [m n]]))
+    it %q{match_stream(nats){ with(Multiset.(_m, _n, *_)) { [m, n] } }.take(20)} do
+      expect(match_stream(nats){ with(Multiset.(_m, _n, *_)) { [m, n] } }.take(20)).to eq \
+        [[1, 2], [1, 3], [2, 1], [1, 4], [2, 3], [3, 1], [1, 5], [2, 4], [3, 2], [4, 1],
+         [1, 6], [2, 5], [3, 4], [4, 2], [5, 1], [1, 7], [2, 6], [3, 5], [4, 3], [5, 2]]
+    end
+
     # (take 10 (match-all nats (set integer) [<cons $m <cons $n _>> [m n]]))
     it %q{match_stream(nats){ with(Set.(_m, _n, *_)) { [m, n] } }.take(10)} do
       expect(match_stream(nats){ with(Set.(_m, _n, *_)) { [m, n] } }.take(10)).to eq \
         [[1, 1], [1, 2], [2, 1], [1, 3], [2, 2], [3, 1], [1, 4], [2, 3], [3, 2], [4, 1]]
+    end
+
+    # (take 20 (match-all nats (set integer) [<cons $m <cons $n _>> [m n]]))
+    it %q{match_stream(nats){ with(Set.(_m, _n, *_)) { [m, n] } }.take(20)} do
+      expect(match_stream(nats){ with(Set.(_m, _n, *_)) { [m, n] } }.take(20)).to eq \
+        [[1, 1], [1, 2], [2, 1], [1, 3], [2, 2], [3, 1], [1, 4], [2, 3], [3, 2], [4, 1],
+         [1, 5], [2, 4], [3, 3], [4, 2], [5, 1], [1, 6], [2, 5], [3, 4], [4, 3], [5, 2]]
     end
   end
 end
